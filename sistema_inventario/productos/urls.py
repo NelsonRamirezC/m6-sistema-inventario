5c123e5359508f723a22b2222414f27a10ed511b
Nelson Ramirez <xhyskoax@gmail.com>
2024-10-16 20:47:39 -0300

    se agrega ejemplo de ruta dinámica que recibe id por url y muestra el detalle de un producto

--- sistema_inventario/productos/urls.py
+++ sistema_inventario/productos/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.productos, name='productos'),
+    path('id/<int:id>', views.detalle_producto, name='detalle-producto'),
 ]
--- sistema_inventario/productos/views.py
+++ sistema_inventario/productos/views.py
@@ -8,10 +8,10 @@ from django.shortcuts import render
 # ]
 
 data_productos = [
-    {"id": 1, "nombre": "Producto 1", "imagen": "producto1.webp", "descripcion": "producto 1"},
-    {"id": 2, "nombre": "Producto 2", "imagen": "producto2.jpeg", "descripcion": "producto 2 producto 2 producto 2 producto 2 "},
-    {"id": 3, "nombre": "Producto 3", "imagen": "producto3.jpeg", "descripcion": "producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3 producto 3"},
-    {"id": 4, "nombre": "Producto 4", "imagen": "producto4.jpeg", "descripcion": "producto 4 producto 4 producto 4 producto 4 producto 4 producto 4 producto 4 producto 4"},
+    {"id": 1, "nombre": "Producto 1", "imagen": "producto1.webp", "descripcion": "Descripción producto 1"},
+    {"id": 2, "nombre": "Producto 2", "imagen": "producto2.jpeg", "descripcion": "Descripción producto 2"},
+    {"id": 3, "nombre": "Producto 3", "imagen": "producto3.jpeg", "descripcion": "Descripción producto 3"},
+    {"id": 4, "nombre": "Producto 4", "imagen": "producto4.jpeg", "descripcion": "Descripción producto 4"},
 ]
 # Create your views here.
 
@@ -20,3 +20,22 @@ def productos(request):
         "productos": data_productos
     }
     return render(request, 'productos/productos.html', contexto)
+
+
+def detalle_producto(request, id):
+    
+    # BUSCAR PRODUCTO POR ID
+    
+    def buscar_producto(datos, id_a_buscar):
+        for producto in datos:
+            if producto["id"] == id_a_buscar:
+                return producto
+        
+    producto = buscar_producto(data_productos, id)
+    
+    contexto = {
+        "id": id,
+        "producto": producto
+    }
+    return render(request, 'productos/detalle_producto.html', contexto)
+
